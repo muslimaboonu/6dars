28fc989d43434de5ffea24815854f975531fefbf
muslima <muslima@gmail.com>
2025-01-10 14:07:39 +0500

    homework

--- app.js
+++ app.js
@@ -1,7 +1,51 @@
-let a = 35;
-let b = 65;
-if (a < b) {
-  for (let i = a; i <= b; i++) {
-    console.log(i);
-  }
-}
+// UYGA VAZIFA
+// 9-mashq("Savol"
+//     For1. a va b butun sonlari berilgan (a < b). a va b sonlari orasidagi barcha butun sonlarni (a va b ni ham) chiqaruvchi va chiqarilgan sonlar sonini chiqaruvchi programma tuzilsin. (a va b xam chiqarilsin).
+//     )
+//let a = 35;
+//let b = 65;
+//if (a < b) {
+// for (let i = b; i > a; i--) {
+//    console.log(i);
+// }
+//}
+
+// 10-mashq("Savol"
+//     For2. a va b butun sonlari berilgan (a < b). a va b sonlari orasidagi barcha butun sonlarni (a va b dan tashqari) kamayish tartibida chiqaruvchi va chiqarilgan sonlar sonini chiqaruvchi progma tuzilsin.
+//     )
+//let a = 35;
+//let b = 65;
+//if (a < b) {
+// for (let i = b; i > a; i--) {
+//    console.log(i);
+// }
+//}
+
+// 11-mashq("Savol" For3. Bir kg konfetning narxi berilgan (haqiqiy son). 1, 2, 10 kg konfetni narxini chiqaruvchi programma tuzilsin.)
+//let konfet = Number(prompt("1kg konfet 16ming sum, necha kg kerak?"));
+//let a = 0;
+//for (let i = 0; i < konfet; i++) {
+//  a++;
+//}
+//alert(`${(a * 16000) / 1000}.ming sum berasiz`);
+
+// 12-mashq("Savol"
+//     For4. Bir kg konfetning narxi berilgan (haqiqiy son). 1.2, 1.4, ..., 2 kg konfetni narxini chiqaruvchi programma tuzilsin.
+//     )
+//let konfet = Number(prompt("1kg konfet 16ming sum necha kg kerak?"));
+//let a = konfet;
+//for (let i = 1; i < konfet; i++) {}
+//alert(`${(a * 16000) / 10} sum bo'ldi`);
+
+// 13-mashq("Savol"
+//     For5. a va b butun sonlari berilgan (a < b). a dan b gacha bo'lgan barcha butun sonlar yig'indisini chiqaruvchi programma tuzilsin.
+//     )
+// let a = 5;
+// let b = 20;
+// let counter = 0;
+// if (a < b) {
+//   for (let i = 0; i <= b; i++) {
+//     counter += i;
+//   }
+// }
+// console.log(counter);
